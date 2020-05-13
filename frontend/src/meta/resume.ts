517ca06dce6655d48dbcce344d996a85fae6ac1a
rau eyal <raueyal@gmail.com>
2020-05-13 16:27:41 +0300

    add form generator

--- frontend/src/meta/resume.ts
+++ frontend/src/meta/resume.ts
@@ -1,5 +1,12 @@
-const ResumeMetadata = {
-    fullName: {
+export interface IResumeStruct{
+    type: string,
+    display: string,
+    regex: string,
+    innerDefinition? : object
+}
+
+export const ResumeMetadata : any  = {
+    fullName : {
         type: "text",
         display: "Full Name",
         regex: ""
@@ -31,5 +38,41 @@ const ResumeMetadata = {
         }
     }
 }
+
+//serving proposal 
+export const ResumeMetadataArr : IResumeStruct[] = [
+    {
+        type: "text",
+        display: "Full Name",
+        regex: ""
+    },
+    {
+        type: "multiline",
+        display: "Summary",
+        regex: ""
+    },
+    {
+        type: "complex",
+        display: "Contact Information",
+        regex: "",
+        innerDefinition: {
+            email: {
+                type: "text",
+                display: "Email",
+                regex: ""
+            },
+            phone: {
+                type: "text",
+                display: "Phone",
+                regex: ""
+            },
+            address: {
+                type: "text",
+                display: "Address",
+                regex: ""
+            }
+        }
+    }
+]
 
 export default ResumeMetadata;
