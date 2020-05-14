a55d3dc43dba9b95d1e47b2802afad26b06e788f
Nissan Cohen <nissanc6@gmail.com>
2020-05-14 13:08:07 +0300

    Creating preview and connecting to resume form

--- frontend/src/components/ResumeOverview.tsx
+++ frontend/src/components/ResumeOverview.tsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Grid } from '@material-ui/core';
 import ResumeForm from './ResumeForm';
+import Preview from './resumePreview/Preview';
+import Resume, { IResume } from '../interfaces/Resume';
 
 const ResumeOverview: React.FC = (props) => {
+    const [resume, setResume] = useState<IResume>(new Resume());
+
+    const setNewResume = (newResume: any) => {
+        console.log("Updating Overview", newResume);
+        setResume(newResume);
+    }
 
     return (
         <Grid container spacing={2}>
             <Grid item xs={12} lg={4}>
-                <ResumeForm />
+                <ResumeForm superResumeUpdater={setNewResume}/>
             </Grid>
             <Grid item xs={12} lg={8}>
-                Overview
+                <Preview resume={resume}/>
             </Grid>
         </Grid>
     );
