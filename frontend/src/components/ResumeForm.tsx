fdddbe5df9aa72ac0f5997d22b274682bab0709a
Nissan Cohen <nissanc6@gmail.com>
2020-05-14 12:37:15 +0300

    Removed form generator and using state correctly

--- frontend/src/components/ResumeForm.tsx
+++ frontend/src/components/ResumeForm.tsx
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from "react";
 import Resume, { IResume } from "../interfaces/Resume";
 import { ResumeApi } from "../api/ResumeApi";
-import TextField from "@material-ui/core/TextField";
-import { makeStyles, Input } from "@material-ui/core";
-import metaResume from "../meta/resume";
-import IResumeStruct from "../meta/resume";
+import { makeStyles } from "@material-ui/core";
 import CV_TextBox from "./formBlocks/CV_TextBox";
+import CV_ContactInformation from "./formBlocks/CV_ContactInformation";
+import ContactInformation, {
+  IContactInformation,
+} from "../interfaces/ContactInformation";
 
 const ResumeForm: React.FC<IResume> = (props) => {
   const [resume, setResume] = useState<IResume>(new Resume());
-
+  const [contactInformation, setContactInformation] = useState<IContactInformation>(new ContactInformation());
+  
   const useStyles = makeStyles(() => ({
     textFieldStyle: {
       margin: "10px",
@@ -29,92 +31,64 @@ const ResumeForm: React.FC<IResume> = (props) => {
 
   const classes = useStyles();
 
-  const formGenerator = (item: any, metadata: any, prefix: string = "", i: number = 0) => {
-    const fields = Object.keys(metadata);
-    console.log(fields);
-    let elements: any[] = [];
-    for (const field of fields) {
-      const fieldMetadata = metadata[field];
-      switch (fieldMetadata.type) {
-        case "text":
-        case "multiline":
-          elements.push(
-            <div key={i++}>
-              <CV_TextBox
-                name={prefix + field}
-                {...fieldMetadata}
-                className={classes.textFieldStyle}
-                value={item && item[field]}
-                onChange={onChangeName}
-              ></CV_TextBox>
-            </div>
-          );
-          break;
-        case "complex":
-          const title = <div key={i++}>{fieldMetadata["display"]}</div>;
-          const innerElements = formGenerator(
-            item[field],
-            fieldMetadata["innerDefinition"],
-            `${field}.`, i
-          );
-          i+= elements.length;
-          const allElements = [title, ...innerElements];
-          console.log("All elements ", allElements);
-          elements = elements.concat(allElements);
-          console.log("After concat", elements);
-          break;
-      }
-    }
-
-    return elements;
-  };
-
   useEffect(() => {
     console.log("using effect!");
 
     fetchResume().then((resm) => {
       setResume(resm);
+      if (resm.contactInformation) {
+        setContactInformation(resm.contactInformation);
+      }
     });
   }, []);
 
-  const changeResume = (part: any, fieldArr: string[], value: any) => {
-    console.log("Rec", part, fieldArr, value)
-    if (fieldArr.length === 0) {
-      return;
-    }
-    if (fieldArr.length === 1) {
-      part[fieldArr[0]] = value;
-      return;
-    }
+  const onChangeResume = (field: string, value: any) => {
+    console.log("Changing", field, "to", value);
+    const newResume = { ...resume, [field]: value };
+    setResume(newResume);
+  };
 
-    const currField = fieldArr[0];
-    fieldArr.splice(0, 1);
-    changeResume(part[currField], fieldArr, value);
-
-    return;
-  }
-
-  const onChangeName = (event: any) => {
+  const onChange = (event: any) => {
+    event.preventDefault();
     console.log(
       "handler trying to change",
       event.target.name,
       "with value:",
       event.target.value
     );
-    const path = event.target.name.split(".");
-    
-    const newResume = {
-       ...resume,
-    };
-    changeResume(newResume, path, event.target.value);
 
-    console.log("New Resume", newResume);
-    setResume(newResume);
+    onChangeResume(event.target.name, event.target.value);
+  };
+
+  const onContactInformationChanged = (event: any) => {
+    event.preventDefault();
+    const newContactInformation = {
+      ...contactInformation,
+      [event.target.name]: event.target.value,
+    };
+
+    setContactInformation(newContactInformation);
+    onChangeResume("contactInformation", newContactInformation);
   };
 
   return (
     <span>
-      <div>{resume && formGenerator(resume, metaResume)}</div>
+      <CV_TextBox
+        name="fullName"
+        display="Full Name"
+        value={resume?.fullName}
+        onChange={onChange}
+      ></CV_TextBox>
+      <CV_TextBox
+        name="summary"
+        display="Summary"
+        value={resume?.summary}
+        onChange={onChange}
+      ></CV_TextBox>
+      <CV_ContactInformation
+        contactInformationState={contactInformation}
+        onContactInformationChanged={onContactInformationChanged}
+      ></CV_ContactInformation>
     </span>
   );
 };
--- frontend/src/components/formBlocks/CV_TextBox.tsx
+++ frontend/src/components/formBlocks/CV_TextBox.tsx
@@ -1,20 +1,20 @@
 import React from "react";
-import { TextField } from "@material-ui/core"
+import { TextField } from "@material-ui/core";
 
-const CV_TextBox = (props : any) => {
-    console.log("CV", props)
-    return (
-        <TextField
-          className={props?.className}
-          label={props?.display}
-          value={props?.value}
-          //helperText=""
-          multiline={props?.type === "multiline"}
-          onChange={props?.onChange}
-          name={props?.name}
-          fullWidth
-        />
-    )
+const CV_TextBox = (props: any) => {
+  return (
+    <div>
+      <TextField
+        className={props?.className}
+        label={props?.display}
+        value={props?.value}
+        multiline={props?.type === "multiline"}
+        onChange={props?.onChange}
+        name={props?.name}
+        fullWidth
+      />
+    </div>
+  );
 };
 
 export default CV_TextBox;
